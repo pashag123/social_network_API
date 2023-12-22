5e0f1698bf4894fcc61d7451dd721cfb70c35b2f
pashag123 <pashag123@gmail.com>
2023-12-22 14:17:05 -0800

    more aded

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,34 @@ module.exports = {
     async getSingleUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v');
+            if(!user) {
+                return res.status(404).json({message: 'no user found with that ID'})
+            }
+            res.json(user)
+        } catch (err) {
+            res.status(500).json(err.message)
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err.message)
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId })
+
+            if(!user) {
+                return res.status(404).json({ message: 'no user with that id'})
+            }
+            await Thought.deleteMany({_id: { $in: user.thoughts}})
+            res.json({ message: 'User and thoughts deleted'})
         } catch (err) {
             res.status(500).json(err.message)
         }
