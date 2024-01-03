1100d3921e76250c657d5afdf21b76ead00a134e
pashag123 <pashag123@gmail.com>
2024-01-03 15:01:26 -0800

    user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { model, Schema, Types } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            unique: true,
+            require: true,
+            trimmed: true,
+
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: []
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        
+        },
+        id: false
+    }
+)
+
+userSchema.virtual("friendCount")
+.get(function () {
+    return this.friends.length
+})
+
+const User = model('user', userSchema)
+
+module.exports = User;
